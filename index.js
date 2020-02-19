b45aa087a425a4d3a0ebd95231c5b46cdf89c3e3
anil battaram <b.anilkumar81@gmail.com>
2020-02-20 04:41:27 +0530

    Testing

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('HELLO Anil...This is development Server of test2 app'));
+app.get('/', (req, res) => res.send('HELLO Anil...This is production Server of test2 app'));
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}`));
